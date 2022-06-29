da57e91c86fe5061729ba795718edcbcf938fa3b
ErleyMartins <wesley.dmartins@outlook.com>
2022-06-29 20:41:10 -0300

    Card Finalizado

---
+++ pro-atividades-app/src/components/Atividade.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+export default function Atividade(props) {
+
+  const prioridade = {
+    '0': {
+      label: 'Não informado',
+      icon: '',
+      color: 'black'
+    },
+    '1': {
+      label: 'Baixa',
+      icon: 'smile',
+      color: 'success'
+    },
+    '2': {
+      label: 'Normal',
+      icon: 'meh',
+      color: 'secondary'
+    },
+    '3': {
+      label: 'Alta',
+      icon: 'frown',
+      color: 'danger'
+    }
+  }
+
+  return (
+    <div className={`card mb-3 shadow-sm border-${prioridade[props.atividade.prioridade].color}`}>
+      <div className="card-body">
+        <div className='d-flex justify-content-between'>
+          <h5 className="card-title">
+            <span className='badge bg-secondary me-1'>
+              { props.atividade.id }
+            </span>
+            { props.atividade.titulo }
+          </h5>
+          <h6 className="card-subtitle mb-2 text-muted">
+            Prioridade:
+            <span className={`ms-1 text-${prioridade[props.atividade.prioridade].color}`}>
+              <i className={`far fa-${prioridade[props.atividade.prioridade].icon} me-1`}></i>
+              { prioridade[props.atividade.prioridade].label }
+            </span>
+          </h6>
+        </div>
+        <p className="card-text">{ props.atividade.descricao }</p>
+        <div className='d-flex justify-content-end pt-2 m-0 border-top'>
+          <button 
+            className='btn btn-sm btn-primary me-1'
+            onClick={() => props.selecionaAtividade(props.atividade.id)}
+          >
+            <i className="fas fa-pen me-1"></i>
+            Editar
+          </button>
+          <button 
+            className='btn btn-sm btn-danger'
+            onClick={() => props.deletarAtividade(props.atividade.id)}
+          >
+            <i className="fas fa-trash me-1"></i>
+            Excluir
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ pro-atividades-app/src/components/AtividadeForm.jsx
@@ -0,0 +1,125 @@
+import React, { useEffect, useState } from 'react'
+
+const atividadeInicial = {
+  id: 0,
+  titulo: '',
+  prioridade: '0',
+  descricao: ''
+}
+
+export default function AtividadeForm(props) {
+  const [atividade, setAtividade] = useState(atividadeAtual());
+
+  useEffect(() => {
+    if (props.atividadeSelecionada.id !== 0) {
+      setAtividade(props.atividadeSelecionada);
+    }
+  }, [props.atividadeSelecionada]);
+
+  function atividadeAtual() {
+    if (props.atividadeSelecionada.id !== 0) {
+      return props.atividadeSelecionada;
+    }
+    else {
+      return atividadeInicial;
+    }
+  }
+
+  const inputTextHandler = (input) => {
+    const { name, value } = input.target;
+
+    setAtividade({...atividade, [name]: value});
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (props.atividadeSelecionada.id !== 0) props.atualizarAtividade(atividade);
+    else props.addAtividade(atividade);
+
+    setAtividade(atividadeInicial);
+  }
+
+  const handlerCancelar = (event) => {
+    event.preventDefault();
+
+    props.cancelarAtividade();
+    setAtividade(atividadeInicial);
+  }
+
+  return (
+    <>
+      <h1>{atividade.id === 0 ? 'Nova Atividade' : `Atividade: ${atividade.id}`}</h1>
+      <form className="row g-3 mb-2" onSubmit={handleSubmit}>
+        <div className="col-md-6">
+          <label className="form-label">Título</label>
+          <input 
+            type="text" 
+            className="form-control" 
+            id="titulo"
+            name="titulo"
+            onChange={inputTextHandler}
+            value={atividade.titulo}
+          />
+        </div>
+        <div className="col-md-6">
+          <label className="form-label">Prioridade</label>
+          <select 
+            id="prioridade"
+            name="prioridade"
+            className="form-select"
+            onChange={inputTextHandler}
+            value={atividade.prioridade}
+          >
+            <option defaultValue='0'>Escolha...</option>
+            <option value='1'>Baixa</option>
+            <option value='2'>Normal</option>
+            <option value='3'>Alta</option>
+          </select>
+        </div>
+        <div className="col-12">
+          <label className="form-label">Descrição</label>
+          <textarea 
+            className='form-control'
+            id="descricao"
+            name="descricao"
+            onChange={inputTextHandler}
+            value={atividade.descricao}
+          ></textarea>
+          <hr />
+        </div>
+        <div className="col-12 mt-0 ">
+          {
+            atividade.id === 0 
+            ? (
+              <button 
+                className="btn btn-success"
+                type='submit'
+              >
+                <i className="fa fa-plus-circle me-1"></i>
+                Adicionar
+              </button>
+            )
+            : 
+            (<>
+                <button 
+                  className="btn btn-success me-2"
+                  type='submit'
+                >
+                  <i className="fa fa-save me-1"></i>
+                  Salvar
+                </button>
+                <button 
+                  onClick={handlerCancelar}
+                  className="btn btn-secondary"
+                >
+                  <i className="fa fa-times-circle me-1"></i>
+                  Cancelar
+                </button>
+              </>)
+          }
+        </div>
+      </form>
+    </>
+  )
+}
